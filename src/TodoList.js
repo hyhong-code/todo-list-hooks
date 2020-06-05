2c3b5d91823c7fb23c7533393bcf7f23fe3191de
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-05 11:12:46 -0700

    add TodoList component

---
+++ src/TodoList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Grid from "@material-ui/core/Grid";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
+
+function TodoList(props) {
+  return (
+    <Paper>
+      <List>
+        {props.todos.map((todo) => (
+          <>
+            <ListItem key={todo.id}>
+              <ListItemText>{todo.task}</ListItemText>
+            </ListItem>
+            <Divider />
+          </>
+        ))}
+      </List>
+    </Paper>
+  );
+}
+
+export default TodoList;
