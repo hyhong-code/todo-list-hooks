8b413863abbde6db83542e33a45bd0e1c4fff8d0
Haiyang Hong <haiyanghong@Haiyangs-iMac.local>
2020-06-05 14:21:30 -0700

    refactor todo functions into own hook

---
+++ src/hooks/useTodoState.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+export default (initialTodos) => {
+  const [todos, setTodos] = useState(initialTodos);
+
+  const addNewTodo = (itemName) => {
+    setTodos([...todos, { id: uuidv4(), task: itemName, completed: false }]);
+  };
+  const removeTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const updateTodo = (id, newTaskName) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, task: newTaskName };
+        }
+        return todo;
+      })
+    );
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      })
+    );
+  };
+
+  return {
+    todos,
+    addNewTodo,
+    removeTodo,
+    updateTodo,
+    toggleTodo,
+  };
+};
